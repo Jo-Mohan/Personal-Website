d29f71451cad2f00564860b2613a3a371749c4e3
Jo-Mohan <jomohan1155@gmail.com>
2025-05-24 12:52:44 -0500

    Landing Page made

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
+import "../index.css"
 
 export default function Landing() {
+  const [showCursor, setShowCursor] = useState(true);
+
+  // when typing animation ends, we remove the caret
+  const handleAnimationEnd = () => {
+    setShowCursor(false);
+  };
+
   return (
-    <div style={{ padding: 40, textAlign: 'center' }}>
-      <h1>Your Name</h1>
-      
+    <div
+      style={{
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        textAlign: 'center',
+        padding: 40,
+      }}
+    >
+      <h1
+        className={`typing ${!showCursor ? 'no-cursor' : ''}`}
+        onAnimationEnd={handleAnimationEnd}
+      >
+        Websites Should Be Simple
+      </h1>
+
       <Link to="/menagerie">
         <button style={{ marginTop: 20, padding: '8px 16px' }}>
-          Enter Menagerie
+          Experiences
         </button>
       </Link>
     </div>
-  )
+  );
 }
